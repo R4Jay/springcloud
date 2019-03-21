ceb2576fa120d06a99f0f43a5e9211926c443e1a
R4Jay <sunxu_2004@163.com>
2019-03-21 17:36:54 +0800

    by sunjx,19.03.21 基于最新SpringCloud Finchley.RELEASE版本提交的

--- springcloud/eurekaserver/src/main/java/com/lynn/EurekaServerApplication.java
+++ springcloud/eurekaserver/src/main/java/com/lynn/EurekaServerApplication.java
@@ -1,18 +1,15 @@
 package com.lynn;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 //EnableEurekaClient，标注了此注解，说明该项目是一个服务提供者。
 @EnableEurekaServer
 @SpringBootApplication
-public class Application  /*extends SpringBootServletInitializer*/ {
+public class EurekaServerApplication  /*extends SpringBootServletInitializer*/ {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
     /*@Override
