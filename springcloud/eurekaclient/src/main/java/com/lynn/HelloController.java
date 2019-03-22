9147616d9df5829c80044d3dc01ba6ab549732d4
R4Jay <sunxu_2004@163.com>
2019-03-22 11:21:28 +0800

    by sunjx,增加3中类型的配置文件

--- springcloud/eurekaclient/src/main/java/com/lynn/HelloController.java
+++ springcloud/eurekaclient/src/main/java/com/lynn/HelloController.java
@@ -1,9 +1,11 @@
 package com.lynn;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
 
@@ -14,4 +16,12 @@ public class HelloController {
     public String index(){
         return "Hello World!,端口："+ port;
     }
+
+    /*@Value("${foo}")
+    String foo;
+
+    @RequestMapping("/foo")
+    public String hi(){
+        return foo;
+    }*/
 }
